e409250967da8c2c3acd689cb92b8ed8aedd2588
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-07-21 18:57:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+
+
+firstMethod();
+
+
+static void firstMethod()
+{
+    Console.WriteLine("Ekrana sevdiğiniz bir şarkı sözünü yazdırıyor");
+}
+
+
+Console.WriteLine("-----------------------------------" +
+    "");
+
+
+
+secondMethod(1,100);
+
+static int secondMethod(int min, int max)
+
+{
+    Random random = new Random();
+   int xRandom= random.Next(min,max+1);
+    int remainder = xRandom % 2;
+
+    Console.WriteLine("Rastgele sayının 2 ye bölümünden kalanı  "+ remainder);
+
+
+    return remainder;
+}
+
+
+Console.WriteLine("-----------------------------------" +
+    "");
+
+
+
+int x= thirdMethod(2,5);
+
+Console.WriteLine("Ikı sayının çarpımının sonucu: " + x);
+
+
+
+    static int thirdMethod(int a,int b)
+{
+
+    return a * b;
+
+}
+
+Console.WriteLine("-----------------------------------" +
+    "");
+
+
+
+fourthMethod ("ahmet ", " can");
+
+static void fourthMethod(string name, string surname)
+{
+    Console.WriteLine("Hoşgeldiniz  " + name + surname);
+}
+
+
